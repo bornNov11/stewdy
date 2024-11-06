b622bc2fc7453d25544199a22760828911b7a589
bornNov11 <lsy882692@gmail.com>
2024-11-06 23:23:30 +0900

    36th commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://stewdy-back.onrender.com';
 
 export default API_URL;
